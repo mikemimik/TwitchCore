d2623c139e5bd15ef9b9670c34a75444662e5640
Michael Perrotte <mike@mikecorp.ca>
2016-01-04 05:59:55 -0500

    legacy changes from master branch [broken]

--- bot.js
+++ bot.js
@@ -2,14 +2,23 @@ var irc = require('irc');
 var config = require('./config').bot;
 
 var bot = new irc.Client(config.server, config.botName, {
-  channels: config.channels
+  channels: config.channels,
+  debug: config.debug
 });
 
 /* Check bot connected to server */
 bot.addListener('raw', function(message) {
-  if (message.rawCommand == '001') {
+  if (message.rawCommand === '001') {
     console.log('Bot connected to server');
   }
+});
+
+bot.addListener('message', function(from, to, message) {
+  console.log('%s => %s: %s', from, to, message);
+});
+
+bot.addListener('error', function(message) {
+  console.log('bot error: ', message);
 });
 
 module.exports = {
--- config.js
+++ config.js
@@ -1,7 +1,32 @@
 module.exports = {
   bot: {
-    channels: ['#cambridge'],
     server: process.env.TCORE_SERVER || 'irc.freenode.net',
-    botName: process.env.TCORE_BOTNAME || 'tcore-bot'
+    botName: process.env.TCORE_NICK || 'collins',
+    userName: process.env.TCORE_USERNAME || 'jcollins',
+    realName: process.env.TCORE_REALNAME || 'JarvisCollins',
+    // port: process.env.TCORE_PORT || 6667,
+    // localAddress: null,
+    debug: process.env.ENV === 'PRODUCTION' ? false : true,
+    // showErrors: false,
+    // autoRejoin: true,
+    // autoConnect: true,
+    channels: ['#cambridge'],
+    // retryCount: 3,
+    // retryDelay: 5000,
+    // secure: false,
+    // selfSigned: false,
+    // certExpired: false,
+    // floodProtection: true,
+    // floodProtectionDelay: 1000,
+    // sasl: false,
+    // stripColors: false,
+    // channelPrefixes: '$#',
+    // messageSplit: 512,
+    // encoding: false,
+    // webirc: {
+      // pass: '',
+      // ip: '',
+      // user: ''
+    // }
   }
-}
+};
